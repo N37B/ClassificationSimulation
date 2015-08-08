0acf27214b4862e23cebd018c2a9ab867c35e573
Daniel Ammons-Moreno <dbammons@gmail.com>
2015-08-08 07:27:22 -0500

    first commit

---
+++ readme.R
@@ -0,0 +1,25 @@
+
+#run script
+
+#user will specify how many runs
+#user will specify discrete number of shippers or go until all shipped 
+
+#import data
+
+#import shippers vs. all test takers
+  #need gender
+
+#import RPM
+  #maybe by month
+
+#import job qualifications
+  #ASVAB Composites
+  #DLAB
+  #Vision
+
+#import job classification behavior
+  #includes choice algorithm  High/Low curves   Choice algotithm should include everyone who chose a job
+  #includes DEP attrite?  Use previous DEP attrites--if model chooses to ship a DA--then DQ the DA
+  #includes not interested in jobs  Use previous quals that didn't pick any job
+
+
